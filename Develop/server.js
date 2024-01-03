78123c4085df480dceee3ebd98ae139a74e31984
TavonnS <142205891+TavonnS@users.noreply.github.com>
2024-01-03 11:57:33 -0800

    seed okay

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
 const { sequelize } = require('./config/connection');
+const Sequelize = require('sequelize');
 
 
 const app = express();
